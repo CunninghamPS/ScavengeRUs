fdd363cd626263854c3877727b5f409835e8d355
CunninghamPS <cunninghamp@etsu.edu>
2022-02-28 21:26:11 -0500

    Main Layout Setup

--- ScavengeRUs/Program.cs
+++ ScavengeRUs/Program.cs
@@ -1,15 +1,12 @@
 //File name: Program.cs
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using ScavengeRUs.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-//Caleb
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
