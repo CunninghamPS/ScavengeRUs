aeda06a2907ceec9f8a0b8db10b9731c699b8ae0
PrestonSC <cunninghamp@etsu.edu>
2022-03-26 20:05:10 -0400

    Adds a basic task list and a model class for games, tasks, and users

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ScavengeRUs.Models
+{
+    public class User
+    {
+        // Set this to the username from the database
+        public string UserName { get; set; }
+
+        // Set this to the list of games from the games table
+        public List<string> currentGames { get; set; }
+
+        // Set this to the list of completed tasks from the user table
+        public List<string> tasksCompleted { get; set; }
+    }
+}
