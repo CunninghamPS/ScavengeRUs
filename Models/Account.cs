27447b1223c89fcb9c8883a49d5d1ac423b156da
Samuel Reynolds <reynoldsse@etsu.edu>
2022-03-31 10:12:10 -0400

    Adds a cohesive UI across all current pages.

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,25 +4,26 @@
     public class Account
     {
         [Required]
-        [StringLength(100, ErrorMessage = "First Name must be shorter than 100 characters")]
+        [StringLength(100, ErrorMessage = "*First name cannot exceed 100 characters")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required]
-        [StringLength(100, ErrorMessage = "Last Name must be shorter than 100 characters")]
+        [StringLength(100, ErrorMessage = "*Last name cannot exceed 100 characters")]
         public string LastName { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression(@"^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$", ErrorMessage="*Date of Birth must be in MM/DD/YYYY\n")]
+        [StringLength(100, ErrorMessage = "*Date of Birth must be in MM/DD/YYYY")]
         public string DOB { get; set; } = string.Empty;
 
         public string TeamName { get; set; } = string.Empty;
 
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "*Please enter a valid email address.\n")]
         public string Email { get; set; } = string.Empty;
 
         [Required]
-        [Phone]
+        [Phone(ErrorMessage="*Please enter a valid phone number.")]
         public string PhoneNumber { get; set; } = string.Empty;
 
         [Required]
@@ -30,7 +31,8 @@
         public string UserName { get; set; } = string.Empty;
 
         [Required]
-
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "*Password must be 8 characters long.\nAt least 1 uppercase and 1 lowercase letter, 1 number and 1 special character.\n")]
+        [StringLength(100, ErrorMessage = "Password cannot exceed 100 characters")]
         public string Password { get; set; } = string.Empty;
     }
 }
