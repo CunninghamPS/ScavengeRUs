3b90d0f1906c0f98ce3cc8baa956b1d21ab4db90
reynoldsse22 <63010181+reynoldsse22@users.noreply.github.com>
2022-04-23 19:40:51 -0400

    Updates the profile and security page

--- Models/Account.cs
+++ Models/Account.cs
@@ -3,34 +3,26 @@
     using System.ComponentModel.DataAnnotations;
     public class Account
     {
-        [Required]
         [StringLength(100, ErrorMessage = "*First name cannot exceed 100 characters")]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required]
         [StringLength(100, ErrorMessage = "*Last name cannot exceed 100 characters")]
         public string LastName { get; set; } = string.Empty;
 
-        [Required]
         [RegularExpression(@"^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$", ErrorMessage="*Date of Birth must be in MM/DD/YYYY\n")]
         [StringLength(100, ErrorMessage = "*Date of Birth must be in MM/DD/YYYY")]
         public string DOB { get; set; } = string.Empty;
 
         public string TeamName { get; set; } = string.Empty;
 
-        [Required]
         [EmailAddress(ErrorMessage = "*Please enter a valid email address.\n")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
         [Phone(ErrorMessage="*Please enter a valid phone number.")]
         public string PhoneNumber { get; set; } = string.Empty;
 
-        [Required]
-
         public string UserName { get; set; } = string.Empty;
 
-        [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "*Password must be 8 characters long.\nAt least 1 uppercase and 1 lowercase letter, 1 number and 1 special character.\n")]
         [StringLength(100, ErrorMessage = "Password cannot exceed 100 characters")]
         public string Password { get; set; } = string.Empty;
--- Pages/UserAccount.razor.cs
+++ Pages/UserAccount.razor.cs
@@ -7,9 +7,20 @@ namespace ScavengeRUs.Pages
     {
         private string inputStyle = "";
         private Account Account = new Account();
+        private bool IsVisible { get; set; } = false;
+        private bool SubmitBtnDisabled { get; set; } = true;
+        private string ErrorMessage { get; set; } = "";
         private void HandleValidSubmit()
         {
-
+            try
+            {
+                DBTest.updateUser(secretKey, Account.FirstName, Account.LastName, Account.UserName, Account.Email, Account.PhoneNumber);
+            }
+            catch (Exception)
+            {
+                inputStyle = "border: .5px solid red";
+            }
+            Console.WriteLine("Valid Submit");
         }
 
         protected override System.Threading.Tasks.Task OnInitializedAsync()
@@ -24,14 +35,39 @@ namespace ScavengeRUs.Pages
             return base.OnInitializedAsync();
         }
 
-        private bool IsVisible { get; set; } = false;
-        private bool SubmitBtnDisabled { get; set; } = true;
-        private string ErrorMessage { get; set; } = "";
+       
 
         private void ClearFormat()
         {
             inputStyle = "";
         }
 
+        private void logout()
+        {
+            if (secretKey != null)
+            {
+                DBTest.logout(secretKey);
+                NavigationManager.NavigateTo("/");
+            }
+            else
+            {
+                DBTest.addUsersToGame();
+            }
+        }
+
+        public async Task<bool> navigateTo(string dest)
+        {
+            string[] paths = { "profile", "security" };
+            string url = NavigationManager.Uri;
+            int length = NavigationManager.Uri.Split('/').Length;
+            string addon = NavigationManager.Uri.Split('/')[length - 1];
+            if (paths.Contains(addon))
+            {
+                NavigationManager.NavigateTo("/" + dest);
+            }
+            else
+                NavigationManager.NavigateTo("/" + dest + "/" + addon);
+            return await System.Threading.Tasks.Task.FromResult(true);
+        }
     }
 }
