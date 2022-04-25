ea4271e0cac25f423e7f056b4a391e936daefbee
reynoldsse22 <63010181+reynoldsse22@users.noreply.github.com>
2022-04-25 01:14:09 -0400

    Fixed validation issues across all pages.
    I also styled invalid input to be more specific across individual text fields. Now an individual text box's border will turn red instead of all of the text boxes

---
+++ Pages/Security.razor.cs
@@ -0,0 +1,135 @@
+﻿using ScavengeRUs.Models;
+using ScavengeRUs.Data;
+using Syncfusion.Blazor.Popups;
+using System.Text.RegularExpressions;
+
+namespace ScavengeRUs.Pages
+{
+    public partial class Security
+    {
+        private string Pass1 = "", Pass2 = "";
+        private string inputStyle = "";
+        private string feedback = "";
+        private string stringOnSubmit = "";
+        private Account Account = new Account();
+        private bool IsVisible { get; set; } = false;
+        private bool SubmitBtnDisabled { get; set; } = true;
+        private ResizeDirection[] dialogResizeDirections { get; set; } = new ResizeDirection[] { ResizeDirection.All };
+
+
+        protected override System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            try
+            {
+                List<string> values = DBTest.getUserInfo(secretKey);
+                Account.FirstName = values[3];
+                Account.LastName = values[4];
+                Account.UserName = values[6];
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error Getting Logged In User Info");
+            }
+            return base.OnInitializedAsync();
+        }
+
+        private void HandleValidSubmit()
+        {
+            Regex rgx = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+            try
+            {
+                if (Pass1.Equals("") || Pass2.Equals(""))
+                    throw new Exception();
+
+                if (!Pass1.Equals(Pass2))
+                    throw new Exception();
+                else
+                {
+                    if (rgx.IsMatch(Pass1))
+                    {
+                        Account.Password = Pass1;
+                        DBTest.updateUserPassword(secretKey, Account.Password);
+                        stringOnSubmit = "Password Updated!";
+                        inputStyle = "";
+                        feedback = "";
+                    }
+                    else
+                    {
+                        feedback = "Password must be 8 characters long with at least 1 uppercase and 1 lowercase letter, 1 number and 1 special character.";
+                        inputStyle = "border: .5px solid red";
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                feedback = "Passwords must match";
+                inputStyle = "border: .5px solid red";
+            }
+            Console.WriteLine("Valid Submit");
+        }
+
+        private void ClearFormat()
+        {
+            inputStyle = "";
+            stringOnSubmit = "";
+        }
+
+        private void deleteUser()
+        {
+            try
+            {
+                DBTest.deleteUserFromDB(secretKey);
+                logout();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot delete user");
+            }
+        }
+        private void logout()
+        {
+            if (secretKey != null)
+            {
+                DBTest.logout(secretKey);
+                NavigationManager.NavigateTo("/");
+            }
+            else
+            {
+                DBTest.addUsersToGame();
+            }
+        }
+
+        void confirmDelete()
+        {
+            OpenDialog();
+        }
+
+        private void OpenDialog()
+        {
+            this.IsVisible = true;
+        }
+
+        private void OnSubmit()
+        {
+            this.IsVisible = false;
+            SubmitBtnDisabled = false;
+        }
+
+        public async Task<bool> navigateTo(string dest)
+        {
+            string[] paths = { "profile", "security" };
+            string url = NavigationManager.Uri;
+            int length = NavigationManager.Uri.Split('/').Length;
+            string addon = NavigationManager.Uri.Split('/')[length - 1];
+            if (paths.Contains(addon))
+            {
+                NavigationManager.NavigateTo("/" + dest);
+            }
+            else
+                NavigationManager.NavigateTo("/" + dest + "/" + addon);
+            return await System.Threading.Tasks.Task.FromResult(true);
+        }
+
+    }
+}
--- Pages/SignUpScreen.razor.cs
+++ Pages/SignUpScreen.razor.cs
@@ -1,32 +1,103 @@
 ﻿using ScavengeRUs.Models;
 using ScavengeRUs.Data;
 using Syncfusion.Blazor.Popups;
+using System.Text.RegularExpressions;
 
 namespace ScavengeRUs.Pages
 {
     public partial class SignUpScreen
     {
         private Models.Account Account = new Account();
-        private string inputStyle = "";
+        private string firstNameStyle = "", lastNameStyle = "", emailStyle = "", phoneStyle = "", passwordStyle = "";
+        private string invalidFirst = "", invalidLast = "", invalidEmail = "", invalidPhone = "", invalidPass = "";
+        private bool validInput = false;
+        Regex passWordRgx = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+        Regex emailRgx = new Regex("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+        Regex phoneNumberRgx = new Regex(@"^(\+\s?)?((?<!\+.*)\(\+?\d+([\s\-\.]?\d+)?\)|\d+)([\s\-\.]?(\(\d+([\s\-\.]?\d+)?\)|\d+))*(\s?(x|ext\.?)\s?\d+)?$");
         private void HandleValidSubmit()
         {
             Random random = new Random();
             Account.UserName = random.Next(111111,999999).ToString();
             try
-            {   
-                if(Account.Email.Equals("") || Account.Password.Equals("") || Account.FirstName.Equals("") || Account.LastName.Equals("") || Account.PhoneNumber.Equals(""))
+            {
+                validInput = true;
+                if(Account.FirstName.Equals("") || Account.FirstName.Length > 100)
                 {
-                    throw new Exception();
+                    invalidFirst = "First name is required.";
+                    if (Account.FirstName.Length > 100)
+                        invalidFirst = "First name cannot exceed 100 characters.";
+
+                    firstNameStyle = "border: .5px solid red";
+                    validInput = false;
                 }
                 else
+                    invalidFirst = "";
+                
+                if (Account.LastName.Equals("") || Account.LastName.Length > 100)
                 {
-                    DBTest.newAccount(Account.Email, Account.DOB, Account.FirstName, Account.LastName, Account.PhoneNumber, Account.UserName, Account.Password);
+                    invalidLast = "Last name is required.";
+                    if (Account.LastName.Length > 100)
+                        invalidLast = "Last name cannot exceed 100 characters.";
+
+                    lastNameStyle = "border: .5px solid red";
+                    validInput = false;
                 }
-                Navigate();
+                else
+                    invalidLast = "";
+                
+                if (Account.Email.Equals("") || !emailRgx.IsMatch(Account.Email))
+                {
+                    invalidEmail = "Please enter a valid email address.";
+                    if (Account.Email.Equals(""))
+                        invalidEmail = "Email address is required.";
+                    
+                    emailStyle = "border: .5px solid red";
+                    validInput = false;
+                }
+                else
+                    invalidEmail = "";
+                
+                if (Account.Password.Equals("") || Account.Password.Length > 100 || !passWordRgx.IsMatch(Account.Password))
+                {
+                    invalidPass = "Password must be 8 characters long with at least 1 uppercase and 1 lowercase letter, 1 number and 1 special character.";
+                    if (Account.Password.Length > 100)
+                        invalidPass = "Password cannot exceed 100 characters.";
+                    
+                    if (Account.Password.Equals(""))
+                        invalidPass = "Password is required.";
+
+                    passwordStyle = "border: .5px solid red";
+                    validInput = false;
+                }
+                else
+                    invalidPass = "";
+                
+                if (Account.PhoneNumber.Equals("") || !phoneNumberRgx.IsMatch(Account.PhoneNumber))
+                {
+                    invalidPhone = "Please enter a valid phone number in the format \"###-###-####\".";
+                    if (Account.PhoneNumber.Equals(""))
+                        invalidPhone = "Phone number is required.";
+                    
+                    phoneStyle = "border: .5px solid red";
+                    validInput = false;
+                }
+                else
+                    invalidPhone = "";
+
+                if (validInput)
+                {
+                    firstNameStyle = "";
+                    lastNameStyle = "";
+                    emailStyle = "";
+                    passwordStyle = "";
+                    phoneStyle = "";
+                    DBTest.newAccount(Account.Email, Account.DOB, Account.FirstName, Account.LastName, Account.PhoneNumber, Account.UserName, Account.Password);
+                    Navigate();
+                }
             }
             catch (Exception)
             {
-                inputStyle = "border: .5px solid red";
+                Console.WriteLine("Invalid Submit");
             }
             Console.WriteLine("Valid Submit");
         }
@@ -58,7 +129,11 @@ namespace ScavengeRUs.Pages
 
         private void ClearFormat()
         {
-            inputStyle = "";
+            firstNameStyle = "";
+            lastNameStyle = "";
+            emailStyle = "";
+            passwordStyle = "";
+            phoneStyle = "";
         }
 
     }
